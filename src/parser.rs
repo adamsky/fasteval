44e4d93f730504809c55c23643a973a188ad9397
adamsky <adamsky@enum.space>
2020-09-23 18:48:11 +0200

    derive serde for Slab and dependent objects

--- src/parser.rs
+++ src/parser.rs
@@ -42,22 +42,26 @@ pub struct ExpressionI(pub usize);
 /// It behaves much like a pointer or reference, but it is 'safe' (unlike a raw
 /// pointer) and is not managed by the Rust borrow checker (unlike a reference).
 #[derive(Debug, PartialEq, Copy, Clone)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct ValueI(pub usize);
 
 /// An `Expression` is the top node of a parsed AST.
 ///
 /// It can be `compile()`d or `eval()`d.
 #[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Expression {
     pub(crate) first: Value,
     pub(crate) pairs: Vec<ExprPair>, // cap=8
 }
 
 #[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub(crate) struct ExprPair(pub BinaryOp, pub Value);
 
 /// A `Value` can be a Constant, a UnaryOp, a StdFunc, or a PrintFunc.
 #[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum Value {
     EConstant(f64),
     EUnaryOp(UnaryOp),
@@ -68,6 +72,7 @@ use Value::{EConstant, EPrintFunc, EStdFunc, EUnaryOp};
 
 /// Unary Operators
 #[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum UnaryOp {
     EPos(ValueI),
     ENeg(ValueI),
@@ -78,6 +83,7 @@ use UnaryOp::{ENeg, ENot, EParentheses, EPos};
 
 /// Binary Operators
 #[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum BinaryOp {
     // Sorted in order of precedence (low-priority to high-priority):
     // Keep this order in-sync with evaler.rs.  (Search for 'rtol' and 'ltor'.)
@@ -100,6 +106,7 @@ use BinaryOp::{EAdd, EDiv, EExp, EMod, EMul, ESub, EAND, EEQ, EGT, EGTE, ELT, EL
 
 /// A Function Call with Standard Syntax.
 #[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum StdFunc {
     EVar(String),
     #[cfg(feature = "unsafe-vars")]
